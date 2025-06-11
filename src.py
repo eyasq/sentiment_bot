0664313e8ed3583964e55d5cca91eb1e4bbec331
eyasq <qeremeyas99@gmail.com>
2025-06-12 00:29:25 +0300

    making sure .gitignore is working properly

--- src.py
+++ src.py
@@ -1,8 +1,8 @@
 import os
 import sys
 import whisper
-
-
+from dotenv import load_dotenv
+from google import genai
 model = whisper.load_model("turbo")
 result=model.transcribe("test_audio/recording.mp3", language="ar")
 
